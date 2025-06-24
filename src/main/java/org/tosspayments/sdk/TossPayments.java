450a3ad685d058cf1b140ef94020f826215a20b3
seate <sa20207@naver.com>
2025-06-24 23:06:37 +0900

    Refactor!: Implement TossPayments's constructors

--- src/main/java/org/tosspayments/sdk/TossPayments.java
+++ src/main/java/org/tosspayments/sdk/TossPayments.java
@@ -1,16 +1,40 @@
 package org.tosspayments.sdk;
 
+import org.tosspayments.sdk.request.requester.Requester;
+import org.tosspayments.sdk.request.requester.RequesterBuilder;
+
 /**
  * Root Module for Toss Payments SDK.
  */
 public class TossPayments {
 
 	/**
-	 * You must document method
-	 * @param aBoolean You must document parameters
-	 * @return You must document return value
+	 * The endpoint URL for Toss Payments API.
+	 * This is the base URL for all API requests.
 	 */
-	public Boolean method(Boolean aBoolean) {
-		return aBoolean;
+	public static final String ENDPOINT = "https://api.tosspayments.com/v1/inform";
+
+
+	private final Requester requester;
+
+	/**
+	 * Creates a new instance of TossPayments with the provided secret key.
+	 * @param secretKey The secret key for authentication with Toss Payments.
+	 */
+	public TossPayments(String secretKey) {
+		this.requester = new RequesterBuilder(ENDPOINT, secretKey)
+			.build();
 	}
+
+	/**
+	 * Creates a new instance of TossPayments with the provided secret key and custom requester.
+	 * @param secretKey The secret key for authentication with Toss Payments.
+	 * @param requester Custom requester for handling requests.
+	 */
+	public TossPayments(String secretKey, Requester requester) {
+		this.requester = new RequesterBuilder(ENDPOINT, secretKey)
+			.withRequester(requester)
+			.build();
+	}
+
 }
